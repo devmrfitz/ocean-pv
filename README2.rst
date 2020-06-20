282c8ac24164b42c33d27b41e85f7a2cbb6c93c0
ignisda <diptesh.choudhuri@gmail.com>
2020-06-20 18:28:40 +0530

    started to add a new project README

---
+++ README2.rst
@@ -0,0 +1,27 @@
+*******************************
+OCEAN-Personality-Visualization
+*******************************
+
+.. image:: https://img.shields.io/github/license/IgnisDa/OCEAN-personality-visualization?style=for-the-badge   
+	:alt: GitHub
+
+A website that helps you visualize your personality using graphs and compare it 
+with others. It asks you a
+series of questions and analyzes your inputs to create an easy to understand 
+graph. It also provides you an
+easy way to share these results with your peers and compare your personalities.
+It is based on the 
+OCEAN_ personality model which is the most acceptable model to measure 
+personality used by researchers. 
+
+.. _OCEAN: https://en.m.wikipedia.org/wiki/Big_Five_personality_traits 
+
+The documentation lives at https://ocean-personality-visualization.readthedocs.io/en/latest/
+
+.. image:: ./project_deps/pictures/single_result_view.png
+    :align: center 
+    :alt: alternate text
+
+What this website does
+======================
+After logging in (or signing up), you're guided to
---
+++ graphs/functions/descriptions/new.py
@@ -0,0 +1,44 @@
+import math
+
+
+def calculate_coordinates(list_of_dictionaries: list) -> list:
+    """ Calculate the coordinates using trigonometry 
+
+    This takes a list of dictionaries of the format `[{'testuser':[6, 4, 3, 10, 5]},
+     {'testuser2':[1, 5, 17, 20, 6]}]` and 
+    then returns a list of coordinates. These coordinates can be used to 
+    represent an irregular polygon whose area 
+    can be calculated using shoelace method. 
+
+    :param list_of_dictionaries: List of dictionaries having key-value of 
+    `{username:[score_list]}`
+
+    :type list_of_dictionaries: list
+
+    :returns: A list of coordinates `[(2, 0.00), (1, 3.34)]` in the format of (x1, y1)
+    :rtype: list """
+
+    score_list = []
+    # for dictionary in list_of_dictionaries:
+    #     score_list.append(list(dictionary.values())[0])
+
+    theta = (2*math.pi)/len(score_list[0])
+
+    area_list=[]
+    for dictionary in list_of_dictionaries:
+        score_list.append(list(dictionary.values())[0])
+
+    coordinate_list = []
+    for score in score_list:
+        coordinates = []
+        for index, coordinate in enumerate(score):
+            angle = (theta/(2*math.pi))*index
+            slope = math.tan(angle)
+            coordinates.append((coordinate, coordinate*slope))
+        coordinate_list.append(coordinates)
+
+    return coordinate_list
+
+
+print(calculate_coordinates([{'testuser': [6, 4, 3, 10, 5]},
+                             {'testuser2': [1, 5, 17, 20, 6]}]))
